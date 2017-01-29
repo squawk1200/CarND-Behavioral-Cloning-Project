786c0f8b1014da1798cac60308c357e36bca69b4
Praveen Sampat <praveen.sampat@gmail.com>
2017-01-28 23:16:21 -0800

    Project submission

---
+++ README.md
@@ -0,0 +1,65 @@
+**1. Architecture**  
+I used the Nvidia pipeline as described in http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf. I made a couple of minor modifications to the pipeline by adding a cropping layer to remove 45 pixels from the top of each image. I did not use a dropout layer.
+   
+The number of layers and sizes for each layer:   
+
+Normalization layer - Output size: (160, 320, 3)  
+Cropping layer 1 - Output size:  (124, 318, 3)  
+Convolution layer 1 - Output size:  (60, 157, 24)  
+Convolution layer 2 - Output size:  (28, 77, 36)  
+Convolution layer 3 - Output size:  (12, 37, 48)  
+Convolution layer 4 - Output size:  (10, 35, 64)  
+Convolution layer 5 - Output size:  (8, 33, 64)  
+Flatten - Output size:  (16896)     
+Fully connected layer 1 - Output size:  (100)  
+Fully connected layer 2 - Output size:  (50)  
+Fully connected layer 3 - Output size:  (10) 
+
+![](sample_images/arch.png) 
+
+
+**2. Data augmentation**  
+I used the Udacity provided data set to train the model. The car could not successfully complete either of the two tracks if I simply trained the data with the Nvidia model. Specifically, the car could not negotiate turns very well. Based on suggestions on the Udacity forums and trial and error, I settled on the following augmentation steps.   
+
+***Discard 50% of the data with steering angle 0***    
+The following histogram shows that the data set was skewed very heavily towards data with steering angle equal to 0. Training on this data set meant that car did not handle curves very well. In each batch, I discarded, at random, 50% of data samples with steering angle 0.    
+
+***Histogram of steering angles*** 
+![](sample_images/steering_hist.png)   
+
+For each data sample, I also selected the image from center, left or right camera with equal probability. I added a small steering offset when the left or right images were selected so as to bias the car to return to the center of the lane. Here are images from the center, left and right cameras. Here are sample images from the left, center and right cameras.
+
+***Left image***
+![](sample_images/left_2.png)
+
+***Center image***
+![](sample_images/center_2.png)
+
+***Right image***
+![](sample_images/right_2.png)
+
+
+***Add random brightness to each image***   
+Since the second track has much different lighting and brightness than track 1, I adjusted each image by a random amount of brightness. This was based on the recommendations made by Vivek Yadav in the post: https://chatbotslife.com/using-augmentation-to-mimic-human-driving-496b569760a9#.8btw7h59y  
+Here is an example of an image with brightness adjustment: 
+ 
+***Original image***
+![](sample_images/center_1.png)
+
+***Brightness adjusted image***
+![](sample_images/brightness_1.png)
+
+***Translate each image vertically & horizontally***    
+The augmentation steps performed so far helped the car complete track 1 successfully and the car did well in track 2 except for one very sharp curve. I added a random translation to each image (again based on the Vivek Yadav post) in the vertical and horizontal direction. Here is an example of a translated image:   
+
+***Original images***
+![](sample_images/center_1.png)![](sample_images/center_2.png)   
+
+***Translated images***
+![](sample_images/trans_1.png)![](sample_images/trans_2.png)
+
+
+***Other training details***   
+I split the data set into a training and validation set (20%). I used a Python generator for generating data samples for each batch. I used the Adam optimizer, batch size of 128 and 5 epochs. I modified drive.py to set the throttle based on current speed and not have a constant throttle. This helps the car not overspeed in down hill situations and helped overcome a steep hill in track 2.   
+
+In my testing, the car is able to successfully navigate both the tracks (screen resolution 640x480, graphics quality: fastest).
---
+++ model.py
@@ -0,0 +1,229 @@
+import base64
+import json
+import numpy as np
+import cv2
+
+from PIL import Image
+from keras.models import Sequential, model_from_json
+from keras.layers.core import Dense, Activation, Flatten, Lambda
+from keras.layers.convolutional import Convolution2D, Cropping2D
+from keras.optimizers import Adam
+
+DATA_DIR                = "data"
+DRIVING_LOG_FILE        = "driving_log.csv"
+WEIGHTS_FILE            = "model.h5"
+MODEL_FILE              = "model.json"
+BATCH_SIZE              = 128
+SAMPLES_PER_EPOCH       = 20000
+NUMBER_OF_EPOCHS        = 5
+STEERING_OFFSET         = 0.25
+ORIG_ROW_SIZE           = 160
+ORIG_COL_SIZE           = 320
+CH                      = 3
+
+
+def load_driving_log(path_to_file):
+    data = np.genfromtxt(path_to_file, dtype=None, delimiter=',', names=True)
+    
+    array_data = []
+    
+    for row in data:
+        array_row = [row[0], row[1], row[2], row[3], row[4], row[5], row[6]]
+        array_data.append(array_row)
+
+    return np.asarray(array_data)
+
+# From Vivek Yadav post
+# https://chatbotslife.com/using-augmentation-to-mimic-human-driving-496b569760a9#.8btw7h59y
+def augment_brightness_camera_images(image):
+    image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+    random_bright = .25+np.random.uniform()
+    image1[:,:,2] = image1[:,:,2]*random_bright
+    image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
+    return image1
+
+# From Vivek Yadav post
+# https://chatbotslife.com/using-augmentation-to-mimic-human-driving-496b569760a9#.8btw7h59y
+def trans_image(image, steer, trans_range):
+    # Translation
+    tr_x = trans_range*np.random.uniform()-trans_range/2
+    steer_ang = steer + tr_x/trans_range*2*.2
+    tr_y = 40*np.random.uniform()-40/2
+    Trans_M = np.float32([[1,0,tr_x], [0,1,tr_y]])
+    image_tr = cv2.warpAffine(image, Trans_M, (ORIG_COL_SIZE, ORIG_ROW_SIZE))
+    
+    return image_tr,steer_ang
+
+
+def process_image(filename, steering_angle):
+    image = Image.open(filename)
+    image = np.asarray(image)
+    # Add random brightness
+    image = augment_brightness_camera_images(image)
+    # Add random translation (horizontal & vertical)
+    image, steering_angle = trans_image(image, steering_angle, 100)
+    
+    # Flip half of the images horizontally
+    if (np.random.randint(2) == 0):
+        image = np.fliplr(image)
+        steering_angle = -steering_angle
+
+    return image, steering_angle
+
+def augment_image(line_item):
+
+    random_select_3 = np.random.randint(3)
+    random_select_2 = np.random.randint(2)
+    
+    steering_angle = line_item[3].astype(float)
+    steering_offset = 0
+    
+    # Discard steering angle 0 data with 50% probability
+    if ((steering_angle == 0.0) and (random_select_2 == 0)):
+        return None, None
+    
+    # Pick image from one of the 3 cameras
+    if (random_select_3 == 0):
+        # Pick center image
+        filename = DATA_DIR + "/" + line_item[0].decode('UTF-8').strip()
+        steering_offset = 0.0
+    elif (random_select_3 == 1):
+        # Pick left image
+        filename = DATA_DIR + "/" + line_item[1].decode('UTF-8').strip()
+        # Bias the car towards the center by adding a positive offset
+        steering_offset = STEERING_OFFSET
+    else:
+        # Pick right image
+        filename = DATA_DIR + "/" + line_item[2].decode('UTF-8').strip()
+        # Bias the car towards the center by adding a negative offset
+        steering_offset = -STEERING_OFFSET
+
+    steering_angle += steering_offset
+    image, steering_angle = process_image(filename, steering_angle)
+
+    return image, steering_angle
+
+
+def batch_generator(driving_log, batch_size=32):
+    batch_images = np.zeros((batch_size, ORIG_ROW_SIZE, ORIG_COL_SIZE, CH))
+    batch_steering_angles = np.zeros(batch_size)
+    numberOfLogEntries = len(driving_log) - 1
+    
+    while 1:
+        for i in range(batch_size):
+            i_line = np.random.randint(numberOfLogEntries)
+            line_item = driving_log[i_line]
+        
+            x, y = augment_image(line_item)
+            
+            while (x is None):
+                i_line = np.random.randint(numberOfLogEntries)
+                line_item = driving_log[i_line]
+                x, y = augment_image(line_item)
+        
+            batch_images[i] = x
+            batch_steering_angles[i] = y
+
+        yield batch_images, batch_steering_angles
+
+
+# Nvidia pipeline
+# http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
+def get_nvidia_model():
+    model = Sequential()
+    
+    # NVIDIA pipeline
+    
+    # 1. Input 160x320x3
+    
+    # 2. Normalization
+    model.add(Lambda(lambda x: x/127.5 - 1.,
+                     input_shape=(ORIG_ROW_SIZE, ORIG_COL_SIZE, CH),
+                     output_shape=(ORIG_ROW_SIZE, ORIG_COL_SIZE, CH)))
+    print("Normalization layer: ", model.layers[-1].output_shape)
+    
+    # Crop 25 pixels from the top of the image
+    model.add(Cropping2D(cropping=((35, 1), (1, 1)),
+                         input_shape=(ORIG_ROW_SIZE, ORIG_COL_SIZE, CH),
+                         dim_ordering='tf'))
+    print("Cropping layer 1: ", model.layers[-1].output_shape)
+    
+    # 3. Convolutional layer 1 5x5 kernel 2x2 stride
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode='valid'))
+    model.add(Activation('relu'))
+    print("Convolution layer 1: ", model.layers[-1].output_shape)
+    
+    # 4. Convolutional layer 2 5x5 kernel 2x2 stride
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode='valid'))
+    model.add(Activation('relu'))
+    print("Convolution layer 2: ", model.layers[-1].output_shape)
+    
+    # 5. Convolutional layer 3 5x5 kernel 2x2 stride
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode='valid'))
+    model.add(Activation('relu'))
+    print("Convolution layer 3: ", model.layers[-1].output_shape)
+    
+    # 6. Convolutional layer 4 3x3 kernel
+    model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+    model.add(Activation('relu'))
+    print("Convolution layer 4: ", model.layers[-1].output_shape)
+    
+    # 7. Convolutional layer 5 3x3 kernel 3x12x64
+    model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+    model.add(Activation('relu'))
+    print("Convolution layer 5: ", model.layers[-1].output_shape)
+    
+    # 8. Flatten
+    model.add(Flatten())
+    print("Flatten: ", model.layers[-1].output_shape)
+    
+    # 9. Fully connected layer 1 - 100 neurons
+    model.add(Dense(100))
+    model.add(Activation('relu'))
+    print("Fully connected layer 1: ", model.layers[-1].output_shape)
+    
+    # 10. Fully connected layer 2 - 50 neurons
+    model.add(Dense(50))
+    model.add(Activation('relu'))
+    print("Fully connected layer 2: ", model.layers[-1].output_shape)
+    
+    # 11. Fully connected layer 3 - 10 neurons
+    model.add(Dense(10))
+    model.add(Activation('relu'))
+    print("Fully connected layer 3: ", model.layers[-1].output_shape)
+    
+    # 12. Output
+    model.add(Dense(1))
+
+    model.compile(optimizer=Adam(), loss="mse")
+    
+    model.summary()
+
+    return model
+
+
+if __name__ == "__main__":
+
+    # Load driving log
+    driving_log = load_driving_log(DATA_DIR + "/" + DRIVING_LOG_FILE)
+
+    # Split into training and validation sets
+    msk = np.random.rand(len(driving_log))  < 0.8
+    train = driving_log[msk]
+    val = driving_log[~msk]
+    
+    model = get_nvidia_model()
+ 
+    # Use a generator - for both training and validation data
+    history = model.fit_generator(batch_generator(train, batch_size=BATCH_SIZE),
+                                  samples_per_epoch=SAMPLES_PER_EPOCH,
+                                  nb_epoch=NUMBER_OF_EPOCHS,
+                                  validation_data=batch_generator(val, batch_size=BATCH_SIZE),
+                                  nb_val_samples=len(val))
+
+    # Save weights
+    model.save_weights(WEIGHTS_FILE)
+                                            
+    # Save model
+    with open(MODEL_FILE, 'w') as outfile:
+        json.dump(model.to_json(), outfile)
